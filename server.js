fd9edb4cdde5df848efe70af5a09a5f9ec3aac77
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-09-04 21:42:29 -0700

    separate teams' stats into sub arrays

--- server.js
+++ server.js
@@ -10,10 +10,33 @@ var app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-request("https://www.teamrankings.com/mlb/stat/1st-inning-scored-percentage", (aerr, res, html) => {
-  console.log("thing")
+const holder = [];
+
+let wholeString;
+
+//This could be a code drill. Break an array up into sub arrays every x indices
+
+request("https://www.teamrankings.com/mlb/stat/1st-inning-scored-percentage", (err, res, html) => {
+  const $ = cheerio.load(html)
+
+  $("table.tr-table").each((i, element) => {
+    const test = $(element).find("tbody").find("td.text-right").text();
+
+    wholeString = test;
+    let stringArr = wholeString.split("%")
+
+    // console.log("hiuwqiurriux", stringArr)
+    let tempArray= []
+    let chunk = 6;
+    for (let i = 0 ; i < stringArr.length; i += chunk) {
+      tempStringArr = stringArr.slice(i, i + chunk);
+      tempArray.push(tempStringArr)
+      // do whatever
+    }
+    console.log(tempArray)
+  })
 })
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
